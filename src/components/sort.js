82d80edfa81aaa8be6ecf00032f3e88edaf10a00
glukomania <glukomani@gmail.com>
2019-09-02 00:16:40 +0200

    Corrections of eslint errors

--- src/components/sort.js
+++ src/components/sort.js
@@ -34,7 +34,7 @@ class Sort extends AbstractComponent {
     `;
   }
 
-  _getSortTemplate({ type, path = `` }) {
+  _getSortTemplate({type, path = ``}) {
     return `
   <div class="trip-sort__item  trip-sort__item--${type}">
     <input id="sort-${type}" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-${type}">
